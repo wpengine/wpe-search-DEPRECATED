e3d5aea33d6ea804ecf1d422bb998245463114eb
Christopher Goldman <dev@oolong.com>
2015-04-06 15:30:58 -0700

    Copied WP_CLI indexing implementation to the API class

--- admin/options.php
+++ admin/options.php
@@ -12,18 +12,30 @@ function ep4wpe_settings_page() {
     <?php settings_fields( 'ep4wpe_elasticsearch_server' ); ?>
     <?php do_settings_sections( 'ep4wpe_settings_page' ); ?>
 
+
   </div>
 <?php
 }
 
 function ep4wpe_elasticsearch_server_section() {
+
+  if( array_key_exists( 'ep_index', $_GET ) && 'true' === $_GET['ep_index'] ) {
+    ep4wpe\ep_index_all();
+    sleep( 1 );
+  }
+
   $stats_map = ep4wpe\ep_stats();
+  $index_button_label = 'Index site';
   if( isset( $stats_map ) ) {
+    $index_button_label = 'Re-index site';
 ?>
     <?php printf( "<div>Search index contains %s documents, utilizing %s of disk space", $stats_map['total']['docs']['count'], size_format( $stats_map['total']['store']['size_in_bytes'], 2 ) ); ?> 
+<?php
+  }
+?>
+  <div><a href="<?php echo admin_url( 'options-general.php?page=ep4wpe-plugin&ep_index=true' ); ?>" class="button secondary-button" onclick="javascript:">Re-index site</a></div>
 
 <?php
-                                                                                         }
 }
 
 function ep4wpe_print_field_callback( $args ) {
@@ -73,13 +85,10 @@ function ep4wpe_settings_init() {
                      array( 'id' => 'ep4wpe_port', 'default' => '9200' )
                      );
 
-
 }
-
 
 /* Add actions, set callbacks */
 
 add_action( 'admin_menu', 'ep4wpe_custom_admin_menu' );
  
 add_action( 'admin_init', 'ep4wpe_settings_init' );
-
