f4424d81ca093004882b3c301737d81daac84809
Christopher Goldman <dev@oolong.com>
2015-04-13 16:43:13 -0700

    012: Added post count setting

--- admin/options.php
+++ admin/options.php
@@ -7,8 +7,11 @@ $s['settings_page_callback'] = 'ep4wpe_settings_page_callback';
 
 $s['settings_group'] = 'ep4wpe-settings';
 
-$s['show_related_post_setting_id'] = ep4wpe\SHOW_RELATED_POSTS_FIELD;
-$s['show_related_post_setting_callback'] = 'ep4wpe_boolean_sanitize';
+$s['show_related_posts_setting_id'] = ep4wpe\SHOW_RELATED_POSTS_FIELD;
+$s['show_related_posts_setting_callback'] = 'ep4wpe_boolean_sanitize';
+
+$s['related_posts_count_setting_id'] = ep4wpe\POST_COUNT_FIELD;
+$s['related_posts_count_setting_callback'] = 'ep4wpe_posts_count_sanitize';
 
 $s['related_posts_section_id'] = 'ep4wpe-related-posts-section';
 $s['related_posts_section_title'] = __('Related Posts');
@@ -16,7 +19,11 @@ $s['related_posts_section_callback'] = 'ep4wpe_related_posts_section_callback';
 
 $s['related_posts_do_show_field_id'] = 'ep4wpe-related-posts-do-show';
 $s['related_posts_do_show_field_title'] = __('Show related posts below single posts');
-$s['related_posts_do_show_field_callback'] = 'ep4wpe_related_posts_field_callback';
+$s['related_posts_do_show_field_callback'] = 'ep4wpe_related_posts_do_show_field_callback';
+
+$s['related_posts_count_field_id'] = 'ep4wpe-related-posts-count';
+$s['related_posts_count_field_title'] = __('Number of related posts to show');
+$s['related_posts_count_field_callback'] = 'ep4wpe_related_posts_count_field_callback';
 
 
 add_action( 'admin_menu', 'ep4wpe_custom_admin_menu' );
@@ -35,12 +42,16 @@ function ep4wpe_custom_admin_menu() {
 add_action( 'admin_init', 'ep4wpe_settings_init' );
 function ep4wpe_settings_init() {
   global $s;
-  register_setting( $s['settings_group'], $s['show_related_post_setting_id'], $s['show_related_post_setting_callback'] );
+  register_setting( $s['settings_group'], $s['show_related_posts_setting_id'], $s['show_related_posts_setting_callback'] );
+  register_setting( $s['settings_group'], $s['related_posts_count_setting_id'], $s['related_posts_count_setting_callback'] );
 
   add_settings_section( $s['related_posts_section_id'], $s['related_posts_section_title'], $s['related_posts_section_callback'], $s['settings_menu_slug'] );
 
   add_settings_field( $s['related_posts_do_show_field_id'], $s['related_posts_do_show_field_title'], $s['related_posts_do_show_field_callback'],
-                      $s['settings_menu_slug'], $s['related_posts_section_id'], [ 'id' => $s['show_related_post_setting_id'], 'default' => false ] ); 
+                      $s['settings_menu_slug'], $s['related_posts_section_id'], [ 'id' => $s['show_related_posts_setting_id'], 'default' => false ] ); 
+
+  add_settings_field( $s['related_posts_count_field_id'], $s['related_posts_count_field_title'], $s['related_posts_count_field_callback'],
+                      $s['settings_menu_slug'], $s['related_posts_section_id'], [ 'id' => $s['related_posts_count_setting_id'], 'default' => false ] ); 
 
 }
 
@@ -77,7 +88,7 @@ function ep4wpe_settings_page_callback() {
 function ep4wpe_related_posts_section_callback() {
 }
 
-function ep4wpe_related_posts_field_callback( $args ) {
+function ep4wpe_related_posts_do_show_field_callback( $args ) {
   $name = $args['id'];
   $checked = checked( 1, get_option( $name ), false );
   echo <<<INPUT
@@ -85,8 +96,24 @@ function ep4wpe_related_posts_field_callback( $args ) {
 INPUT;
 }
 
+function ep4wpe_related_posts_count_field_callback( $args ) {
+  global $s;
+  $value = ep4wpe_posts_count_sanitize( get_site_option( ep4wpe\POST_COUNT_FIELD ) );
+  echo <<<INPUT
+<input name="${s['related_posts_count_setting_id']}" id="${s['related_posts_count_setting_id']}" type="text" value="${value}" class="code"/>
+INPUT;
+}
+
 function ep4wpe_boolean_sanitize( $input ) {
   return $input == true;
+}
+
+function ep4wpe_posts_count_sanitize( $input ) {
+  $sanitized = intval( $input );
+  if( $sanitized == 0 || $sanitized > ep4wpe\MAX_RELATED_POSTS ) {
+    $sanitized = ep4wpe\MAX_RELATED_POSTS;
+  }
+  return $sanitized;
 }
 
 
